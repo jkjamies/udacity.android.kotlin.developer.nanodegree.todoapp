352f7fa28c559947b4bf83e76e95fb98c5b646b4
jkjamies <jkjamies@mtu.edu>
2021-01-11 23:02:49 -0500

    test cleanup

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -1,6 +1,7 @@
 package com.example.android.architecture.blueprints.todoapp.statistics
 
 import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.hamcrest.CoreMatchers.`is`
 import org.junit.Assert.*
 import org.junit.Test
 
@@ -14,51 +15,71 @@ class StatisticsUtilsTest {
                 Task("title", "description", isCompleted = false)
         )
 
+        // When the list of tasks is computed with an active task
         val result = getActiveAndCompletedStats(tasks)
 
-        assertEquals(0f, result.completedTasksPercent)
-        assertEquals(100f, result.activeTasksPercent)
+        // Then the percentages are 100 and 0
+        assertThat(result.activeTasksPercent, `is`(100f))
+        assertThat(result.completedTasksPercent, `is`(0f))
+    }
+
+    // If there's 1 completed tasks and 0 active tasks,
+    // then there are 100% completed tasks and 0% active tasks.
+    @Test
+    fun getActiveAndCompletedStats_noActive_returnsZeroHundred() {
+        val tasks = listOf(
+                Task("title", "description", isCompleted = true)
+        )
+        // When the list of tasks is computed with a completed task
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Then the percentages are 0 and 100
+        assertThat(result.activeTasksPercent, `is`(0f))
+        assertThat(result.completedTasksPercent, `is`(100f))
     }
 
     // If there's 2 completed tasks and 3 active tasks,
     // then there are 40% completed tasks and 60% active tasks.
     @Test
     fun getActiveAndCompletedStats_notCompleted_returnsFortySixty() {
+        // Given 3 completed tasks and 2 active tasks
         val tasks = listOf<Task>(
                 Task("title", "description", isCompleted = true),
                 Task("title", "description", isCompleted = true),
-                Task("title", "description", isCompleted = false),
+                Task("title", "description", isCompleted = true),
                 Task("title", "description", isCompleted = false),
                 Task("title", "description", isCompleted = false)
         )
 
+        // When the list of tasks is computed
         val result = getActiveAndCompletedStats(tasks)
 
-        assertEquals(40f, result.completedTasksPercent)
-        assertEquals(60f, result.activeTasksPercent)
+        // Then the result is 40-60
+        assertThat(result.activeTasksPercent, `is`(40f))
+        assertThat(result.completedTasksPercent, `is`(60f))
     }
 
     // If there's 0 completed tasks and 0 active tasks,
     // then there are 0% completed tasks and 0% active tasks.
     @Test
     fun getActiveAndCompletedStats_notCompleted_returnsZeros() {
-        val tasks = listOf<Task>()
+        // When there are no tasks
+        val result = getActiveAndCompletedStats(emptyList())
 
-        val result = getActiveAndCompletedStats(tasks)
-
-        assertEquals(0f, result.completedTasksPercent)
-        assertEquals(0f, result.activeTasksPercent)
+        // Both active and completed tasks are 0
+        assertThat(result.activeTasksPercent, `is`(0f))
+        assertThat(result.completedTasksPercent, `is`(0f))
     }
 
 
     // If tasks are null, then there are 0% completed tasks and 0% active tasks.
     @Test
     fun getActiveAndCompletedStats_error_returnsZeros() {
-        val tasks = null
+        // When there's an error loading stats
+        val result = getActiveAndCompletedStats(null)
 
-        val result = getActiveAndCompletedStats(tasks)
-
-        assertEquals(0f, result.completedTasksPercent)
-        assertEquals(0f, result.activeTasksPercent)
+        // Both active and completed tasks are 0
+        assertThat(result.activeTasksPercent, `is`(0f))
+        assertThat(result.completedTasksPercent, `is`(0f))
     }
 }
