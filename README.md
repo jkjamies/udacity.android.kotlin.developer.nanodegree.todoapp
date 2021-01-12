50505a20a1696fea4599cb91fb971fabff3b9ecb
jkjamies <jkjamies@mtu.edu>
2021-01-11 22:49:46 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # udacity.android.kotlin.developer.nanodegree.todoapp
 ToDoApp Application from Udacity Android Kotlin Developer Nanodegree program.
+
+Highlights:
+
+Using and Running Tests via file on line bar, Run window, file menu, and Android File/package explorer\
+\
+Test (Local tests) vs AndroidTest (Instrumented tests) (Test is faster with lower fidelity = how real world the test is, AndroidTest has real or emulated devices and is slower with more fidelity)\
+\
+Alt+Enter on function can generate test, or right click->generate->test\
+\
---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -0,0 +1,40 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.junit.Assert.*
+import org.junit.Test
+
+class StatisticsUtilsTest {
+
+    // If there's 0 completed tasks and 1 active tasks,
+    // then there are 0% completed tasks and 100% active tasks.
+    @Test
+    fun getActiveAndCompletedStats_notCompleted_returnsHundredZero() {
+        val tasks = listOf<Task>(
+                Task("title", "description", isCompleted = false)
+        )
+
+        val result = getActiveAndCompletedStats(tasks)
+
+        assertEquals(0f, result.completedTasksPercent)
+        assertEquals(100f, result.activeTasksPercent)
+    }
+
+    // If there's 2 completed tasks and 3 active tasks,
+    // then there are 40% completed tasks and 60% active tasks.
+    @Test
+    fun getActiveAndCompletedStats_notCompleted_returnsFortySixty() {
+        val tasks = listOf<Task>(
+                Task("title", "description", isCompleted = true),
+                Task("title", "description", isCompleted = true),
+                Task("title", "description", isCompleted = false),
+                Task("title", "description", isCompleted = false),
+                Task("title", "description", isCompleted = false)
+        )
+
+        val result = getActiveAndCompletedStats(tasks)
+
+        assertEquals(40f, result.completedTasksPercent)
+        assertEquals(60f, result.activeTasksPercent)
+    }
+}
